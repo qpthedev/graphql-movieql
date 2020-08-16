edf8ee69d7e9936609263e90f210b57695d516eb
qpthedev <qpthedev@gmail.com>
2020-08-16 09:12:16 -0400

    dependency update, movie api link update

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,7 +2,7 @@ import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
     movie: (_, { id }) => getMovie(id),
     suggestions: (_, { id }) => getSuggestions(id),
   },
